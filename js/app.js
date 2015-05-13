03e69a6d5d542ed4678974298cc9f312354253ea
Ethan Rivas <ethan-rivas@hotmail.com>
2015-05-12 19:36:31 -0500

    Estilo, Stage y Layer

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// Primero Creamos el Stage
+var stage = new Konva.Stage({
+    container: 'container',
+    width: 400,
+    height: 400
+});
+// Creamos el Layer
+var layer = new Konva.Layer();
+// Añadimos el Layer al Stage
+stage.add(layer);
